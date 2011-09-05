157b5492af6b4596e807cc2eacef102c73747bb8
Patrick Stadler <patrick.stadler@gmail.com>
2011-09-05 16:37:09 +0200

    server.js formatting

--- server.js
+++ server.js
@@ -8,21 +8,21 @@ http.createServer(function (request, response) {
 	// http-bind proxy
 	if(request.url === '/http-bind/') {
 		var proxy_req = http.request({ host: config.http_bind.host, port: config.http_bind.port, path: config.http_bind.path, method: request.method });
-	  proxy_req.on('response', function(proxy_response) {
-	    proxy_response.addListener('data', function(chunk) {
-	      response.write(chunk, 'binary');
-	    });
-	    proxy_req.on('end', function() {
-	      response.end();
-	    });
-	    response.writeHead(proxy_response.statusCode, proxy_response.headers);
-	  });
-	  request.addListener('data', function(chunk) {
-	    proxy_req.write(chunk, 'binary');
-	  });
-	  request.addListener('end', function() {
-	    proxy_req.end();
-	  });
+		proxy_req.on('response', function(proxy_response) {
+			proxy_response.addListener('data', function(chunk) {
+				response.write(chunk, 'binary');
+			});
+			proxy_req.on('end', function() {
+				response.end();
+			});
+			response.writeHead(proxy_response.statusCode, proxy_response.headers);
+		});
+		request.addListener('data', function(chunk) {
+			proxy_req.write(chunk, 'binary');
+		});
+		request.addListener('end', function() {
+			proxy_req.end();
+		});
 	
 	// static files
 	} else {	
