36e7f4c69db4cecd29868c1b375085d5ef8e5774
Patrick Stadler <patrick.stadler@gmail.com>
2011-09-05 16:29:31 +0200

    first running version

--- server.js
+++ server.js
@@ -1,5 +1,43 @@
-var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\nApp (candy) is running..');
-}).listen(11772);
+var config = require('./config'),
+	fs = require('fs'),
+	http = require('http'),
+	staticServer = new(require('node-static').Server)('./public');
+
+http.createServer(function (request, response) {
+	
+	// http-bind proxy
+	if(request.url === '/http-bind/') {
+		var proxy_req = http.request({ host: config.http_bind.host, port: config.http_bind.port, path: config.http_bind.path, method: request.method });
+	  proxy_req.on('response', function(proxy_response) {
+	    proxy_response.addListener('data', function(chunk) {
+	      response.write(chunk, 'binary');
+	    });
+	    proxy_req.on('end', function() {
+	      response.end();
+	    });
+	    response.writeHead(proxy_response.statusCode, proxy_response.headers);
+	  });
+	  request.addListener('data', function(chunk) {
+	    proxy_req.write(chunk, 'binary');
+	  });
+	  request.addListener('end', function() {
+	    proxy_req.end();
+	  });
+	
+	// static files
+	} else {	
+		request.addListener('end', function() {
+			if(request.url === '/' || request.url === '/index.html') {
+				// index file
+				fs.readFile('public/index.html', 'ascii', function(err, data) {
+					data = data.replace('OPTIONS', JSON.stringify(config.candy));
+					response.write(data, 'ascii');
+					response.end();
+				});
+			} else {
+				staticServer.serve(request, response);
+			}
+		});		
+	}
+	
+}).listen(8080);
